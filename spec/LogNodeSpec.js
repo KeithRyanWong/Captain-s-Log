a2722795f6362d0b11d83e62f26991c75fbedaa5
Keith Wong <keithryanwong@gmail.com>
2018-06-21 20:49:02 -0700

    Test LogNode

--- spec/LogNodeSpec.js
+++ spec/LogNodeSpec.js
@@ -5,7 +5,6 @@ describe("LogNode", () => {
     let time = Date.now();
 
     describe("#LogNode", () => {
-        
         it("initiates with an empty string and the current time", () => {
             expect(node.createdTime/100).toBeCloseTo(time/100);
             expect(node.text).toBe("");
@@ -20,20 +19,20 @@ describe("LogNode", () => {
         it("adds subsequent text on a newline", () => {
             node.appendText("This is a new entry.");
             node.appendText("This is another new entry!");
-            expect(node.text).toBe('Hello World!\nThis is a new entry\nThis is another new entry');
+            expect(node.text).toBe('Hello World!\nThis is a new entry.\nThis is another new entry!');
         });
         it("does not add an empty entry", () => {
             let node2 = new LogNode();
             node2.appendText("");
-            expect(node.text).toBe("");
+            expect(node2.text).toBe("");
             node.appendText("");
-            expect(node.text).toBe('Hello World!\nThis is a new entry\nThis is another new entry');
+            expect(node.text).toBe('Hello World!\nThis is a new entry.\nThis is another new entry!');
         });
     });
     
     describe("#updateText", () => {
         it("overwrites existing text", () => {
-            expect(node.text).toBe('Hello World!\nThis is a new entry\nThis is another new entry');
+            expect(node.text).toBe('Hello World!\nThis is a new entry.\nThis is another new entry!');
             node.updateText("Bye Rihanna");
             expect(node.text).toBe("Bye Rihanna");
             node.updateText("");
--- src/LogNode.js
+++ src/LogNode.js
@@ -1,13 +1,18 @@
 export default function LogNode() {
-    // this.createdTime = Date.now();
-    // this.txt = "";
+    this.createdTime = Date.now();
+    this.text = "";
 }
 
 LogNode.prototype.appendText = function (txt) {
-    // this.txt += "\n" + txt;
+    if (txt == "") return;
+    if (this.text === "") {
+        this.text += txt;
+    } else {
+        this.text += "\n" + txt;
+    }
 };
 
 //Need to figure out how this works with editing text
 LogNode.prototype.updateText = function(txt) {
-    // this.txt = txt;
+    this.text = txt;
 };
